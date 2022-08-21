aa5ce8afd0a3b1a89f9e0c7cb452d535a0cb405b
FaBiUsKcomp <fabio@zeester.com.br>
2022-08-20 22:50:14 -0300

    Fix endpoint URL.

--- src/services/api.cofig.ts
+++ src/services/api.cofig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthToken } from "utils/useAuth";
 
 const instance = axios.create({
-  baseURL: "https://soft-drive-backend.herokuapp.com",
+  baseURL: "https://sd-drive-backend.herokuapp.com",
   // baseURL: "http://localhost:4000",
   headers: { Authorization: `Bearer ${getAuthToken()}` },
 });
